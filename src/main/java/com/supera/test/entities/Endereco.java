a8600fe5cb8038453ece7885d9262f2c845e7289
M4TH3US17 <matheusdalvino50@gmail.com>
2022-02-19 00:30:56 -0400

    Entidade Endereco Criada

---
+++ src/main/java/com/supera/test/entities/Endereco.java
@@ -0,0 +1,34 @@
+package com.supera.test.entities;
+
+import java.io.Serializable;
+
+public class Endereco implements Serializable {
+private static final long serialVersionUID = 1L;
+	
+	private String street;
+	private Integer numberHouse;
+	
+	public Endereco() {
+	}
+
+	public Endereco(String street, Integer numberHouse) {
+		this.street = street;
+		this.numberHouse = numberHouse;
+	}
+	
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public Integer getNumberHouse() {
+		return numberHouse;
+	}
+
+	public void setNumberHouse(Integer numberHouse) {
+		this.numberHouse = numberHouse;
+	}
+}
