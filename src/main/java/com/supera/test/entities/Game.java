2afc436d1c97311b13d564854a94ae5c4e59c7b2
M4TH3US17 <matheusdalvino50@gmail.com>
2022-02-19 00:57:34 -0400

    Implementacao da classe Game

--- src/main/java/com/supera/test/entities/Game.java
+++ src/main/java/com/supera/test/entities/Game.java
@@ -1,10 +1,97 @@
 package com.supera.test.entities;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_games")
-public class Game {
+public class Game implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String urlImagem;
+	private String descricao;
+	private Double preco;
+	
+	public Game() {
+	}
 
+	public Game(Long id) {
+		this.id = id;
+	}
+	
+	public Game(Long id, String nome, String urlImagem, String descricao, Double preco) {
+		this.id = id;
+		this.nome = nome;
+		this.urlImagem = urlImagem;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+	}
+	
 }
