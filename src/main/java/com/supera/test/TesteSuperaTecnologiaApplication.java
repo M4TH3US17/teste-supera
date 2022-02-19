194a06c0934b33def452d4e8e847f8b2423cc121
M4TH3US17 <matheusdalvino50@gmail.com>
2022-02-19 00:21:30 -0400

    Projeto Criado

---
+++ src/main/java/com/supera/test/TesteSuperaTecnologiaApplication.java
@@ -0,0 +1,13 @@
+package com.supera.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSuperaTecnologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSuperaTecnologiaApplication.class, args);
+	}
+
+}
