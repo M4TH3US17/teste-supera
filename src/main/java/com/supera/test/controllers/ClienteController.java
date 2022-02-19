e4b49c794af83f7be8e1ffcfa755db993a4719ba
M4TH3US17 <matheusdalvino50@gmail.com>
2022-02-19 01:54:51 -0400

    Implementacao ClienteController

--- src/main/java/com/supera/test/controllers/ClienteController.java
+++ src/main/java/com/supera/test/controllers/ClienteController.java
@@ -1,10 +1,56 @@
 package com.supera.test.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.supera.test.entities.Cliente;
+import com.supera.test.services.ClienteService;
 
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
-
+	
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Cliente>> findAll(){
+		List<Cliente> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Cliente> findById(@PathVariable Long id){
+		Cliente obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping(consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Cliente> insert(@RequestBody Cliente obj){
+		obj = service.insert(obj);
+		return ResponseEntity.status(HttpStatus.CREATED).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+	
+	@PutMapping(value = "/{id}", produces = "application/json", consumes = "application/json")
+	public ResponseEntity<Cliente> update(@PathVariable Long id, @RequestBody Cliente obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
 }
